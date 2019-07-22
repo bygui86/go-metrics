d3885967e46354d60e0a74c0e10dc67040f194a2
Matteo Baiguini <baiguini.matteo@gmail.com>
2019-07-22 11:54:09 +0200

    Added service implementation

---
+++ rest/config.go
@@ -0,0 +1,94 @@
+package rest
+
+import (
+	"github.com/bygui86/go-metrics/utils"
+	"github.com/bygui86/go-metrics/utils/logger"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+const (
+	// Environment variables -
+	restHostEnvVar            = "ECHOSERVER_REST_HOST"
+	restPortEnvVar            = "ECHOSERVER_REST_PORT"
+	restShutdownTimeoutEnvVar = "ECHOSERVER_REST_SHUTDOWN_TIMEOUT"
+
+	// Default values -
+	// host values: '0.0.0.0' for kubernetes, 'localhost' for local
+	restHostDefault            = "localhost"
+	restPortDefault            = 7001
+	restShutdownTimeoutDefault = 15
+
+	// Custom metrics -
+	opsProcessedMetricName = "echoserver_processed_ops_total"
+	opsProcessedMetricHelp = "Total number of processed operations"
+	echoRequestsMetricName = "echoserver_echo_requests_total"
+	echoRequestsMetricHelp = "Total number of default echo requests"
+	echoMsgRequestsMetricName = "echoserver_echo_msg_requests_total"
+	echoMsgRequestsMetricHelp = "Total number of echo requests with messages"
+)
+
+// Config -
+type Config struct {
+	RestHost        string
+	RestPort        int
+	ShutdownTimeout int
+	CustomMetrics   *CustomMetrics
+}
+
+// CustomMetrics - Container object for custom metrics
+type CustomMetrics struct {
+	opsProcessed prometheus.Counter
+	echoRequests prometheus.Counter
+	echoMsgRequests prometheus.Counter
+}
+
+// newConfig -
+func newConfig() (*Config, error) {
+
+	logger.Log.Debugln("[REST] Setup new REST server config...")
+
+	return &Config{
+		RestHost:        utils.GetStringEnv(restHostEnvVar, restHostDefault),
+		RestPort:        utils.GetIntEnv(restPortEnvVar, restPortDefault),
+		ShutdownTimeout: utils.GetIntEnv(restShutdownTimeoutEnvVar, restShutdownTimeoutDefault),
+		CustomMetrics:   newCustomMetrics(),
+	}, nil
+}
+
+// NewCustomMetrics -
+func newCustomMetrics() *CustomMetrics {
+
+	return &CustomMetrics{
+		opsProcessed: promauto.NewCounter(prometheus.CounterOpts{
+			Name: opsProcessedMetricName,
+			Help: opsProcessedMetricHelp,
+		}),
+		echoRequests: promauto.NewCounter(prometheus.CounterOpts{
+			Name: echoRequestsMetricName,
+			Help: echoRequestsMetricHelp,
+		}),
+		echoMsgRequests: promauto.NewCounter(prometheus.CounterOpts{
+			Name: echoMsgRequestsMetricName,
+			Help: echoMsgRequestsMetricHelp,
+		}),
+	}
+}
+
+// IncrementOpsProcessed - Increment total number of processed operations
+func (cm *CustomMetrics) incrementOpsProcessed() {
+
+	cm.opsProcessed.Inc()
+}
+
+// incrementEchoRequests - Increment total number of default echo requests
+func (cm *CustomMetrics) incrementEchoRequests() {
+
+	cm.echoRequests.Inc()
+}
+
+// incrementEchoMsgRequests - Increment total number of echo requests with message
+func (cm *CustomMetrics) incrementEchoMsgRequests() {
+
+	cm.echoMsgRequests.Inc()
+}
